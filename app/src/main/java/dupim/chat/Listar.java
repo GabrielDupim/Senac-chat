457e67abc05866539666ebd9dd16df5da5ade8cd
Gabriel Dupim <gabriel.dsantos13@senacsp.edu.br>
2022-03-31 11:07:07 -0300

    Primeiro Commit

---
+++ app/src/main/java/dupim/chat/Listar.java
@@ -0,0 +1,15 @@
+package dupim.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar);
+    }
+}
+
